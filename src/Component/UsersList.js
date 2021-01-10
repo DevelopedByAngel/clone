a26c78d60a1bd0bc41696bd1d4aa38815839166f
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-10 12:32:03 +0530

    styling

--- src/Component/UsersList.js
+++ src/Component/UsersList.js
@@ -1,17 +1,18 @@
-import React,{Component} from 'react';
-import UserInList from './UserInList.js'
-class UsersList extends Component
-{
-
-  render()
-  {
-    return(
-     this.props.users.map((u,i)=>
-     {
-     	return	<UserInList key={i} fun={this.props.fun} user={u} userID={this.props.userID}/>
-     })
-    )
-  }
+import React, { Component } from "react";
+import UserInList from "./UserInList.js";
+class UsersList extends Component {
+	render() {
+		return this.props.users.map((u, i) => {
+			return (
+				<UserInList
+					key={i}
+					fun={this.props.fun}
+					user={u}
+					userID={this.props.userID}
+				/>
+			);
+		});
+	}
 }
 
 export default UsersList;
