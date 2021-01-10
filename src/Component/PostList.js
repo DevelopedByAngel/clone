5a34a38b7c4f2eaed345d7cf434f029d59befbb5
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-10 12:30:50 +0530

    styling

--- src/Component/PostList.js
+++ src/Component/PostList.js
@@ -1,47 +1,53 @@
-import React,{Component} from 'react';
-import Post from './Post.js'
-import $ from 'jquery';
-class PostList extends Component
-{
-	componentDidMount()
-	{
-		
-		if(this.props.fun.state.route==="profile")
-		{    
-    		$('.PostList').attr('class', 'PostList profilePostList');
-    		$('.Post').attr('class','Post profilePost');
-		}
-		else
-		{
-			$('.PostList').attr('class', 'PostList');
-			$('.Post').attr('class', 'Post')
+import React, { Component } from "react";
+import Post from "./Post.js";
+import $ from "jquery";
+class PostList extends Component {
+	componentDidMount() {
+		if (this.props.fun.state.route === "profile") {
+			$(".PostList").attr("class", "PostList profilePostList");
+			$(".Post").attr("class", "Post profilePost");
+		} else {
+			$(".PostList").attr("class", "PostList");
+			$(".Post").attr("class", "Post");
 		}
 	}
-  render()
-  {
-    return(
-     this.props.postList.reverse().map(d=>
-     {
-     	var li=d.caption.split(' ')
-		li=li.map((c)=>
-		{
-			var a=c
-			if(c[0]==='#')
-			{
-				a='<a className="hashtag" style="color:blue;text-decoration:none" href="/hashtag/'+c.replace("#","")+'">'+c.replace("#","")+'</a>'
-				return a
-			}
-			else
-			{
-				return c
-			}
-
-		})
-		var caption = li.join(' ')
-     	return	<Post fun={this.props.fun} post={d} path={d.path} key={d._id} user={d.user} postno={d._id} uid={this.props.user.id} caption={caption} like={d.likes.filter(this.props.fun.onlyUnique).length} likes={d.likes} comment={d.comments.length} comments={d.comments} share={d.noOfShare}/>
-     })
-    )
-  }
+	render() {
+		return this.props.postList.reverse().map((d) => {
+			var li = d.caption.split(" ");
+			li = li.map((c) => {
+				var a = c;
+				if (c[0] === "#") {
+					a =
+						'<a className="hashtag" style="color:blue;text-decoration:none" href="/hashtag/' +
+						c.replace("#", "") +
+						'">' +
+						c.replace("#", "") +
+						"</a>";
+					return a;
+				} else {
+					return c;
+				}
+			});
+			var caption = li.join(" ");
+			return (
+				<Post
+					fun={this.props.fun}
+					post={d}
+					path={d.path}
+					key={d._id}
+					user={d.user}
+					postno={d._id}
+					uid={this.props.user.id}
+					caption={caption}
+					like={d.likes.filter(this.props.fun.onlyUnique).length}
+					likes={d.likes}
+					comment={d.comments.length}
+					comments={d.comments}
+					share={d.noOfShare}
+				/>
+			);
+		});
+	}
 }
 
 export default PostList;
