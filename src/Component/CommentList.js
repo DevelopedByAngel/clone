93255fdfec4c0957c75dbffc7276dc3bce481906
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-10 12:29:50 +0530

    styling

--- src/Component/CommentList.js
+++ src/Component/CommentList.js
@@ -1,18 +1,25 @@
-import React,{Component} from 'react';
-import Comment from './Comment.js'
+import React, { Component } from "react";
+import Comment from "./Comment.js";
 
-class CommentList extends Component
-{
-  render()
-  {
-
-    return(
-     this.props.comments.reverse().map((d,i)=>
-     {
-     	return	<Comment fun={this.props.fun} key={d._id} user={d.user} id={d._id} uid={this.props.fun.state.user.id} comment={d.comment} like={d.likes} likes={d.like}  reply={d.replies} index={this.props.fun.state.post._id+"_"+i}/>
-     })
-    )
-  }
+class CommentList extends Component {
+	render() {
+		return this.props.comments.reverse().map((d, i) => {
+			return (
+				<Comment
+					fun={this.props.fun}
+					key={d._id}
+					user={d.user}
+					id={d._id}
+					uid={this.props.fun.state.user.id}
+					comment={d.comment}
+					like={d.likes}
+					likes={d.like}
+					reply={d.replies}
+					index={this.props.fun.state.post._id + "_" + i}
+				/>
+			);
+		});
+	}
 }
 
 export default CommentList;
