f3af3a41bec1abfa89d550e8ab2cad6920a6d75d
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-10 12:30:15 +0530

    styling

--- src/Component/Loader.js
+++ src/Component/Loader.js
@@ -1,18 +1,15 @@
-import React,{Component} from 'react';
-import $ from 'jquery';
-import "../stylesheet/Loader.css"
+import React, { Component } from "react";
+import $ from "jquery";
+import "../stylesheet/Loader.css";
 import PuffLoader from "react-spinners/PuffLoader";
-class Loader extends Component
-{
-		
-  render()
-  {
-    return(
-    	<div className="Loader">
-    	<PuffLoader color="#ffffff" loading={this.props.loading}/>
-    	</div>
-    )
-  }
+class Loader extends Component {
+	render() {
+		return (
+			<div className="Loader">
+				<PuffLoader color="#ffffff" loading={this.props.loading} />
+			</div>
+		);
+	}
 }
 
 export default Loader;
