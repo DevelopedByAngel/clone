9c2694240d01ebdc73ea1e5d708653100de203ef
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-10 12:30:06 +0530

    styling

--- src/Component/FriendsList.js
+++ src/Component/FriendsList.js
@@ -1,17 +1,11 @@
-import React,{Component} from 'react';
-import Friends from './Friends.js'
-class FriendsList extends Component
-{
-
-  render()
-  {
-    return(
-     this.props.users.map((u,i)=>
-     {
-     	return	<Friends key={i} id={i} user={u} />
-     })
-    )
-  }
+import React, { Component } from "react";
+import Friends from "./Friends.js";
+class FriendsList extends Component {
+	render() {
+		return this.props.users.map((u, i) => {
+			return <Friends key={i} id={i} user={u} />;
+		});
+	}
 }
 
 export default FriendsList;
