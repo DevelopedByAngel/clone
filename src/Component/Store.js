d3569e16daa18c9565bfde69f87391a96e79ca89
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-22 11:11:27 +0530

    adding store component

--- src/Component/Store.js
+++ src/Component/Store.js
@@ -9,6 +9,11 @@ const Store = (props) => {
 					<input type="text" className="auth" />
 				</form>
 			</div>
+			<div className="drop">
+			</div>
+			<svg className="drop-svg" id="svg">
+			<path d="M0 0 L10 10 L50 30 Z"
+			</svg>
 		</div>
 	);
 };
