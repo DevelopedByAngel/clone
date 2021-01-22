982b4e64f612800f0319f102218f94752766ef15
angel francis <50880391+Angelfrancis@users.noreply.github.com>
2021-01-22 11:00:17 +0530

    adding store component

--- src/Component/Store.js
+++ src/Component/Store.js
@@ -1,15 +1,16 @@
-import React,{Component} from 'react';
-import $ from 'jquery';
-import  '../stylesheet/Menu.css'
-const Store=(props)=>
-{
-	
+import React, { Component } from "react";
+import $ from "jquery";
+import "../stylesheet/Menu.css";
+const Store = (props) => {
 	return (
-	      <div className="Store">
-		       </div>
-	    );
-
-}
+		<div className="Store">
+			<div className="auth-div">
+				<form className="auth-form">
+					<input type="text" className="auth" />
+				</form>
+			</div>
+		</div>
+	);
+};
 
 export default Store;
-
